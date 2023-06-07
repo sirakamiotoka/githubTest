ebe49fd47d5fa20889344e8c0dee898b4c9c1422
sirakamiotoka <tiruno0415@gmail.com>
2023-06-07 22:00:54 +0900

    マイナスボタン追加

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ window.addEventListener("load", () => {
   let n = 0;
   let countnum, plusbutton;
   plusbutton = document.getElementById("plus-button");
+  minusbutton = document.getElementById("minus-button");
+
   countnum = document.getElementById("count-num");
   function addCount() {
     n++;
     countnum.innerHTML = n;
   }
+  function minusCount() {
+    n--;
+    countnum.innerHTML = n;
+  }
+  minusbutton.addEventListener("click", minusCount);
   plusbutton.addEventListener("click", addCount);
 });
